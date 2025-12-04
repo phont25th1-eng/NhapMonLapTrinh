e74d78ad1b6b9456c254db4cac2548e7af28e9b6
buoi5 <Administrator@PMT2028>
2025-12-04 13:03:55 +0700

    buoi5

--- buoi5/buoi5.cpp
+++ buoi5/buoi5.cpp
@@ -15,7 +15,6 @@ void cau1() {
     if (c > a && c > b)
     cout << c;
 }
-
 void cau2() {
     float a, b;
     cout << " Nhap a, b: ";
@@ -24,11 +23,24 @@ void cau2() {
         cout << "cung dau";
     else
         cout << "khac dau";
+} 
+void cau3() {
+    int a, b;
+    cout << "Nhap a, b: ";
+    cin >> a >> b;
+    if (a == 0) {
+        if (b == 0)
+            cout << "Phuong trinh vo so nghiem" << endl;
+        else
+            cout << "Phuong trinh vo nghiem" << endl;
+    }
+    else {
+        cout << "Nghiem la:" << (-b * 1.0 / a) << endl;
+    }
 }
-
 
 int main()
 {
-    cau2();
+    cau3();
 }
 
