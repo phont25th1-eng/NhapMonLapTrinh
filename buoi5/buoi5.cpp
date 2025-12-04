9ffe8585a512a5ddc6554d4735ee2da54550f267
buoi4 <Administrator@PMT2028>
2025-12-04 11:00:11 +0700

    buoi5

---
+++ buoi5/buoi5.cpp
@@ -0,0 +1,28 @@
+
+
+#include <iostream>
+using namespace std;
+
+void cau1() {
+    int a, b, c;
+    cout << " Nhap a, b, c: ";
+    cin >> a >> b >> c;
+
+    if (a > b && a > c);
+    cout << a;
+    if (b > a && b > c);
+    cout << b;
+    if (c > a && c > b);
+    cout << c;
+}
+
+void cau2() {
+
+}
+
+
+int main()
+{
+    cau1();
+}
+
